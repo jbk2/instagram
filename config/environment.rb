7ff6ed8b4e7837cd5378fcd52aa0a240a7b96feb
James B Kemp <jamesbkemp@me.com>
2015-02-09 11:32:39 +0000

    secret key method of loading a.gitignored heroku_env.rb file before Rails.initialize

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require File.join(File.dirname(__FILE__), 'boot')
+
+# Load heroku vars from local file
+heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+load(heroku_env) if File.exists?(heroku_env)
+
+Rails::Initializer.run do |config|
 
 # Initialize the Rails application.
 Rails.application.initialize!
