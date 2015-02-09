a1ceeeb6d1b555bddde1f52e076299742a3ad0c9
James B Kemp <jamesbkemp@me.com>
2015-02-09 11:55:16 +0000

    could only get 'Tamersaleh's'secret keys method to work for production envs so now reverting back

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,13 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require File.join(File.dirname(__FILE__), 'boot')
 
-# Load heroku vars from local file
-heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
-load(heroku_env) if File.exists?(heroku_env)
+# following http://tammersaleh.com/posts/managing-heroku-environment-variables-for-local-development/
+# suggestions on setting up secret keys
+# require File.join(File.dirname(__FILE__), 'boot')
+
+# # Load heroku vars from local file
+# heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+# load(heroku_env) if File.exists?(heroku_env)
 
 # Initialize the Rails application.
 Rails.application.initialize!
