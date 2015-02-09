64f4a5f87826716b2570e8034d27d6664d0454bf
James B Kemp <jamesbkemp@me.com>
2015-02-09 15:23:16 +0000

    removing local ENVs and corrected syntax in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
     storage: :s3,
     s3_credentials: {
       bucket: 'instagram-clone-jbk',
-      access_key_id: ENV['S3_ACCESS_KEY']
+      access_key_id: ENV['S3_ACCESS_KEY'],
       secret_access_key: ENV['S3_SECRET_KEY']
     }
 
